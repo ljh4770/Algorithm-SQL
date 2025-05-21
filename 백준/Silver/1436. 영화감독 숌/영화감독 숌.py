954943a6ad1d37a22e6a17e72566af594355ef34
JungHwan Lee <70630521+ljh4770@users.noreply.github.com>
2025-05-21 18:51:23 +0900

    [Silver V] Title: 영화감독 숌, Time: 648 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+cnt = 0
+i = 0
+while True:
+    if "666" in str(i):
+        cnt += 1
+    
+    if cnt == n:
+        print(i)
+        break
+    i += 1
