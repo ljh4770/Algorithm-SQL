9cd3303139c93e60b736a10bcad214e70ad59eda
JungHwan Lee <70630521+ljh4770@users.noreply.github.com>
2025-05-21 18:45:54 +0900

    [Silver II] Title: 제곱수의 합, Time: 5504 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+dp = [x for x in range (n + 1)]
+
+for i in range(1, n + 1, 1):
+    for j in range(1, i, 1):
+        if j * j > i :
+            break
+        if dp[i] > dp[i - j * j] + 1 :
+            dp[i] = dp[i - j * j] + 1
+print(dp[n])
