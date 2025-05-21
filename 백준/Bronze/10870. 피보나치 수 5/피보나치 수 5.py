a311d5b5037aa80e3cbaf264495425290af000a2
JungHwan Lee <70630521+ljh4770@users.noreply.github.com>
2025-05-21 17:57:28 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,23 @@
+def fib_dp(n):
+    if n <= 1:
+        return n
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+
+def fib_optimized(n):
+    if n <= 1:
+        return n
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+if __name__ == '__main__':
+    import sys
+
+    n = int(sys.stdin.readline())
+    print(fib_optimized(n))
+
