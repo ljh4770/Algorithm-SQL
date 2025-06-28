6ad6f4a6352b6b5e2a9e16049c1e7248ae7b2580
JungHwan Lee <70630521+ljh4770@users.noreply.github.com>
2025-06-28 23:21:14 +0900

    [Silver V] Title: 거스름돈, Time: 72 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+dp = [-1] * (n + 8)
+dp[2] = 1
+dp[4] = 2
+dp[5] = 1
+dp[6] = 3
+dp[7] = 2
+dp[8] = 4
+
+for i in range(9, n + 1):
+    dp[i] = min(dp[i - 2], dp[i - 5]) + 1
+
+print(dp[n])
