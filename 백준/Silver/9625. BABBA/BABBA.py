3bb8ab613a08c1aa6c5d725f9de895bbf8a2f461
JungHwan Lee <70630521+ljh4770@users.noreply.github.com>
2025-07-07 11:41:45 +0900

    [Silver V] Title: BABBA, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.py
@@ -0,0 +1,10 @@
+k = int(input())
+
+dp = [[0, 0] for _ in range(k + 1)]
+dp[0] = [1, 0]
+
+for i in range(1, k + 1, 1):
+    dp[i][0] = dp[i - 1][1]
+    dp[i][1] = dp[i - 1][1] + dp[i - 1][0]
+
+print(dp[k][0], dp[k][1])
