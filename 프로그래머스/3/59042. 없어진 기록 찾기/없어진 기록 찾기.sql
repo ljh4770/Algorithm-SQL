19a8e8474d37d728c6d2b684e586406043e67efb
JungHwan Lee <70630521+ljh4770@users.noreply.github.com>
2025-09-04 11:41:19 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,15 @@
+/*
+일부 데이터가 유실되었습니다.
+
+입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물
+
+ ID 순으로 조회
+*/
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_OUTS
+WHERE ANIMAL_ID NOT IN (
+    SELECT ANIMAL_ID
+    FROM ANIMAL_INS
+)
+ORDER BY ANIMAL_ID ASC;
