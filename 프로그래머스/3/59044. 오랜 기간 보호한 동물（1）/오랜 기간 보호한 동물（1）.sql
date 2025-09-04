6bcdf4288f737184e9e63c5a43ded2fb564f5a02
JungHwan Lee <70630521+ljh4770@users.noreply.github.com>
2025-09-04 11:37:43 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,13 @@
+/*
+아직 입양을 못 간 동물 중,
+가장 오래 보호소에 있었던 동물 3마리
+*/
+
+SELECT NAME, DATETIME
+FROM ANIMAL_INS
+WHERE ANIMAl_ID NOT IN (
+    SELECT ANIMAL_ID
+    FROM ANIMAL_OUTS
+)
+ORDER BY DATETIME ASC
+LIMIT 3;
