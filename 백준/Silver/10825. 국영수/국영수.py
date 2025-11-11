48fbc017502aca97b5fa0018237e6aa3a91183c1
JungHwan Lee <70630521+ljh4770@users.noreply.github.com>
2025-11-11 23:52:05 +0900

    [Silver IV] Title: 국영수, Time: 348 ms, Memory: 83912 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,8 @@
+import sys; input = sys.stdin.readline
+
+n = int(input())
+student = [input().split() for _ in range(n)]
+student.sort(key = lambda x:(-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for i in student:
+  print(i[0])
